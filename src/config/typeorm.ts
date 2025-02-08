0be77de68b85a9a18795f8d8ef05fdd224b74201
angel-iscoding <angel.iscoding@gmail.com>
2025-02-08 01:03:13 -0500

    Agregado variables de entorno

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,14 +1,14 @@
 import { registerAs } from "@nestjs/config";
-import { config as dotennvConfig } from "dotenv";
+import { config as dotenvConfig } from "dotenv";
 import { DataSource, DataSourceOptions, Migration } from "typeorm";
 
 const config = {
     type: 'postgres',
-    database: 'demodb',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'admin',
+    database: process.env.DATABASE_NAME || 'demodb',
+    host: process.env.DATABASE_HOST || 'db',
+    port: Number(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'admin',
     autoLoadEntities: true,
     synchronize: true,
     dropSchema: true, // Cambia esto a false para evitar perder datos en cada reinicio
