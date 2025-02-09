790af91fc9dc39f423b7f0da581fac2de91792b1
angel-iscoding <angel.iscoding@gmail.com>
2025-02-08 23:18:22 -0500

    Agregando endpoint add

--- src/store-management/cart/cart.controller.ts
+++ src/store-management/cart/cart.controller.ts
@@ -1,17 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, InternalServerErrorException, NotFoundException, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { Cart } from './cart.entity';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { DateAdderInterceptor } from 'src/utils/interceptors/date-adder.interceptor';
+import { UsersService } from 'src/user-management/users/user.service';
 
 @ApiTags('Cart')
 @Controller('cart')
 export class CartController {
-    constructor(private readonly cartService: CartService) {}
+    constructor(
+        private readonly cartService: CartService,
+        private readonly usersService: UsersService
+    
+    ) {}
 
     @Get()
     @ApiOperation({ summary: 'Get all carts' })
     @ApiResponse({ status: 200, description: 'List of all shopping carts' })
-    async GetAllCart(): Promise<Cart[]> {
+    async getAllCart(): Promise<Cart[]> {
         return await this.cartService.getAllCart();
     }
+
+    @Get(':id')
+    async getCart(@Param() id: string): Promise<Cart> {
+        return await this.cartService.getCart(id);
+    }
+
+    @Put('add')
+    @UseGuards(AuthGuard)
+    @UseInterceptors(DateAdderInterceptor)
+    @ApiOperation({ summary: 'Agregar un producto al carrito del usuario' })
+    @ApiResponse({ status: 200, description: 'Producto agregado exitosamente' })
+    @ApiResponse({ status: 400, description: 'Error al agregar el producto' })
+    async addProduct(@Body() id: string, productId: string[]): Promise<{message: string}> {
+        try {
+            const cart = await this.cartService.addProductToUserCart(id, productId);
+            return { message: cart.id }
+        } catch (error) {
+            throw new BadRequestException('No se pudo hacer la solicitud' + error.message);
+        }
+    }
+
+    /* @Delete('delete')
+    async deleteProduct(@Body() id: string, productId: string[]): Promise<{ message: string }> {
+        try {
+            const cart = await this.cartService.deleteProductOfUserCart(id, productId);
+            return { message: cart.id }
+        } catch (error) {
+            throw new BadRequestException('No se pudo hacer la solicitud' + error.message);
+        }
+    } */
 }
--- src/store-management/cart/cart.service.ts
+++ src/store-management/cart/cart.service.ts
@@ -2,18 +2,65 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Cart } from './cart.entity';
 import { CartRepository } from './cart.repository';
 import { UsersRepository } from 'src/user-management/users/user.repository';
-import { NotFoundError } from 'rxjs';
+import { ProductsRepository } from '../products/product.repository';
 
 @Injectable()
 export class CartService {
     constructor(
         private cartRepository: CartRepository,
         private userRepository: UsersRepository,
+        private productRepostory: ProductsRepository,
     ) {}
 
     async getAllCart(): Promise<Cart[]> {
         return await this.cartRepository.getAllCart();
     }
+
+    async getCart(id: string): Promise<Cart> {
+        return await this.cartRepository.getCartById(id);
+    }
+
+    async getCartByUser(id: string): Promise<Cart | undefined> {
+        const user = await this.userRepository.getUserById(id);
+        if (!user) throw new NotFoundException('Usuario no encontrado')
+        
+        return user.cart
+    }
+
+    async addProductToUserCart(id: string, productId: string[]): Promise<Cart> {
+        const user = await this.userRepository.getUserById(id)
+
+        if (!user) throw new NotFoundException ('Error al encontrar el usuario');
+
+        const validProducts = await Promise.all(productId.map(async (currentProduct) => {    
+            const product = await this.productRepostory.getProductById(currentProduct);
+            if (!product) return
+            return product
+        }));
+
+        for (const currentProduct of validProducts) {
+            if (currentProduct) {
+                user.cart.products.push(currentProduct);
+            }
+        }
+
+        // Recalcular el precio total del carrito
+        user.cart.price = user.cart.products.reduce((total, currentProduct) => {
+            return total + currentProduct.price;
+        }, 0);
+
+        return await this.cartRepository.save(user.cart);
+    }
+
+    /* async deleteProductOfCart(id: string, productId: string[]): Promise<void> {
+        const user = await this.userRepository.getUserById(id)
+
+        if (!user) throw new NotFoundException ('Error al encontrar el usuario');
+
+        const newCart = await Promise.all(user.cart.products.map(async (currentProduct) => {
+
+        }))
+    } */
 
     async getAllProductsOfUserCart(userId: string): Promise<string[]> {
         const user = await this.userRepository.getUserById(userId);
