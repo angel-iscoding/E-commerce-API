2adfaba7d96e1402669a757bfc68551f2b6de10a
angel-iscoding <angel.iscoding@gmail.com>
2025-02-17 16:03:42 -0500

    Mejoradas descripciones de Swagger

--- src/database/auth/sign-in.dto.ts
+++ src/database/auth/sign-in.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignInDto {
+    @ApiProperty({ 
+        example: 'john.doe@example.com',
+        description: 'Email del usuario'
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({ 
+        example: 'Password123!',
+        description: 'Contraseña del usuario'
+    })
     @IsNotEmpty()
     password: string;
 }
