9e3188c3768f44264b96bfae984a6eec5b6b94be
angel-iscoding <angel.iscoding@gmail.com>
2025-02-18 22:03:38 -0500

    Deleted propety User from the DTO

--- src/database/cart/cartDto.dto.ts
+++ src/database/cart/cartDto.dto.ts
@@ -2,11 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class cartDto {
-    @ApiProperty({ description: 'ID del usuario o ID temporal', example: 'user123' })
-    @IsOptional()
-    @IsString()
-    userId: string;
-
     @IsArray()
     @IsNotEmpty()
     @ApiProperty({ 
