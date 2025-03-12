7323fce3a5bf3166de74b4f418b9c54681d06331
angel-iscoding <angel.iscoding@gmail.com>
2025-03-11 23:22:10 -0500

    Added Dto verification in payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { Role } from 'src/config/role.enum';
 import { User } from 'src/user-management/users/user.entity';
+import { PayloadDto } from 'src/database/users/payload.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,24 +17,25 @@ export class AuthService {
     async signUp(signUpDto: SignUpDto): Promise<Omit<User, 'password'> | undefined> {
         const hashedPassword = await bcrypt.hash(signUpDto.password, 10);
         
-        const isAdmin = signUpDto.role === Role.Admin;
-        
-        return await this.usersService.createUser({ 
+        const isAdmin: boolean = signUpDto.role === Role.Admin;
+
+        const user: User = await this.usersService.createUser({ 
             ...signUpDto, 
             password: hashedPassword,
             admin: isAdmin
-        });
+        }); 
+        
+        return user;
     }
 
     async signIn(email: string, password: string): Promise<string | null> {
-        const user = await this.usersService.findByEmail(email);
+        const user: User = await this.usersService.findByEmail(email);
 
         if (!user || !(await bcrypt.compare(password, user.password))) {
             throw new BadRequestException('Email o contraseña incorrectos');
         }
 
-        const payload = { email: user.email, id: user.id, roles: user.admin ? [Role.Admin] : [Role.User] };
-    
+        const payload: PayloadDto = { email: user.email, id: user.id, roles: user.admin ? [Role.Admin] : [Role.User] };
         return this.jwtService.sign(payload, { secret: `${process.env.JWT_SECRET}` });
     }
 }
