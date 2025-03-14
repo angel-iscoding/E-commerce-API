fc9b5beaeec315dfcde9464004c82d37f33e911e
angel-iscoding <angel.iscoding@gmail.com>
2025-03-13 20:33:26 -0500

    Not necesary

--- src/database/cart/removeFromCartDto.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class RemoveFromCartDto {
-    @ApiProperty({ description: 'ID del usuario o ID temporal', example: 'user123' })
-    userId: string;
-
-    @ApiProperty({ description: 'ID del producto a remover', example: 'product123' })
-    productId: string;
-}
